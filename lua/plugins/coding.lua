409b8f90f2bdb12f43aef2f1d7dac67e3515f731
yancy <yyancy517@gmail.com>
2023-11-26 23:05:49 +0800

    feat: change b textobject

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -4,4 +4,12 @@ return {
     cmd = "IncRename",
     config = true,
   },
+  {
+    "echasnovski/mini.ai",
+    opts = {
+      custom_textobjects = {
+        b = { { "%b()", "%b[]", "%b{}", '%b""', "%b''", "%b<>" }, "^.().*().$" },
+      },
+    },
+  },
 }
