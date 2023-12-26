12b89660d9412e0fea6466419283873527a8bf08
dongyang <dongyang@wondertek.com.cn>
2023-12-26 18:21:54 +0800

    feat: add new textojbect and enable git blame line

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -272,4 +272,11 @@ return {
       })
     end,
   },
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      current_line_blame = true,
+      current_line_blame_opts = { delay = 1000, virtual_text_pos = "eol" },
+    },
+  },
 }
