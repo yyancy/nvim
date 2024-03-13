b490e2c4aaf0f5a0959705176dcd73257a3234e2
yancy <yyancy517@gmail.com>
2024-03-13 23:50:38 +0800

    add bigfile.nvim plugin

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -5,4 +5,25 @@ return {
     lazy = false,
   },
   { "tpope/vim-repeat", lazy = false },
+  {
+    "LunarVim/bigfile.nvim",
+    opts = {
+      features = {
+        "indent_blankline",
+        "illuminate",
+        "treesitter",
+        "lsp",
+        "syntax",
+      },
+      pattern = function(bufnr, filesize_mib)
+        -- you can't use `nvim_buf_line_count` because this runs on BufReadPre
+        local file_contents = vim.fn.readfile(vim.api.nvim_buf_get_name(bufnr))
+        local file_length = #file_contents
+        local filetype = vim.filetype.match({ buf = bufnr })
+        if file_length > 8000 then
+          return true
+        end
+      end,
+    },
+  },
 }
