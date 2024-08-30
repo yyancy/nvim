ccd367718424d486fe2f891c3a9f82af87ce503b
dongyang <dongyang@wondertek.com.cn>
2024-08-30 10:24:30 +0800

    fix wrong wrap position when set gj and gk mapping

--- lua/config/vscode.lua
+++ lua/config/vscode.lua
@@ -3,13 +3,17 @@ local opts = { noremap = true, silent = true }
 local ropts = { remap = true, silent = true }
 local vscode = require("vscode-neovim")
 
+-- options
+vim.opt.ve = "onemore"
+
+-- keymaps
 set("n", "<c-j>", "<c-w>j", ropts)
 set("n", "<c-k>", "<c-w>k", ropts)
 set("n", "<c-h>", "<c-w>h", ropts)
 set("n", "<c-l>", "<c-w>l", ropts)
 
--- set("n", "j", "gj", ropts)
--- set("n", "k", "gk", ropts)
+set("n", "j", "gj", ropts)
+set("n", "k", "gk", ropts)
 
 
 -- lsp
