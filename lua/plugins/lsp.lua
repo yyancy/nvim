7026ca5e92fe4f6ef5b204477e0131efa4a9d410
yancy <yyancy517@gmail.com>
2024-09-08 23:29:48 +0800

    Add golang plugin

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,18 +1,36 @@
 return {
-  "neovim/nvim-lspconfig",
-  init = function()
-    local keys = require("lazyvim.plugins.lsp.keymaps").get()
-    -- change a keymap
-    -- keys[#keys + 1] = {
-    --   "gd",
-    --   function()
-    --     require("telescope.builtin").lsp_definitions({ reuse_win = true })
-    --     vim.cmd("norm! zz")
-    --   end,
-    -- }
-    -- disable a keymap
-    -- keys[#keys + 1] = { "K", false }
-    -- add a keymap
-    -- keys[#keys + 1] = { "H", "<cmd>echo 'hello'<cr>" }
-  end,
+  {
+    "neovim/nvim-lspconfig",
+    init = function()
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+      -- change a keymap
+      -- keys[#keys + 1] = {
+      --   "gd",
+      --   function()
+      --     require("telescope.builtin").lsp_definitions({ reuse_win = true })
+      --     vim.cmd("norm! zz")
+      --   end,
+      -- }
+      -- disable a keymap
+      -- keys[#keys + 1] = { "K", false }
+      -- add a keymap
+      -- keys[#keys + 1] = { "H", "<cmd>echo 'hello'<cr>" }
+    end,
+  },
+
+  -- golang
+  {
+    "ray-x/go.nvim",
+    dependencies = { -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    event = { "CmdlineEnter" },
+    ft = { "go", "gomod" },
+    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+  },
 }
