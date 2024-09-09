08862c6febec15d03c9a8bebb4f84ad952e383ec
dongyang <dongyang@wondertek.com.cn>
2024-09-09 20:00:35 +0800

    Updates

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -33,3 +33,35 @@ Make_repeatable_keymap = function(mode, lhs, rhs)
   end)
   return lhs
 end
+
+-- after yanking comtent, keep cursor position
+-- https://github.com/svban/YankAssassin.nvim/blob/main/lua/YankAssassin/init.lua
+local pre_yank_pos = {}
+-- Function to save the cursor position before yanking
+local function pre_yank_motion()
+  pre_yank_pos = vim.api.nvim_win_get_cursor(0)
+end
+
+-- Function to restore the cursor position after yanking
+local function post_yank_motion()
+  vim.api.nvim_win_set_cursor(0, pre_yank_pos)
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter", "CursorMoved" }, {
+  callback = function()
+    pre_yank_motion()
+  end,
+})
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    -- Only restore position after yanked with 'y' operator only
+    -- If not set, text yanked with c will also activate it
+    local operators = { "y" } -- Add more operators here if needed
+    if vim.tbl_contains(operators, vim.v.event.operator) then
+      local myMode = vim.api.nvim_get_mode().mode
+      if (myMode == "no") or (myMode == "n") then
+        post_yank_motion()
+      end
+    end
+  end,
+})
--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -192,7 +192,7 @@ return {
           enabled = false,
         },
         char = {
-          enabled = true,
+          enabled = false,
           jump_labels = true,
           multi_line = false,
           label = {
