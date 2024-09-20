8812303f55a2435d3fa2e52e7db1cdc4b047f681
dongyang <dongyang@wondertek.com.cn>
2024-09-20 17:49:24 +0800

    Fix wrong indentation textobject

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -80,6 +80,45 @@ return {
 
           return { from = { line = line_num, col = from_col }, to = { line = line_num, col = to_col } }
         end,
+        i = function(ai_type)
+          local spaces = (" "):rep(vim.o.tabstop)
+          local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+          local indents = {} ---@type {line: number, indent: number,tabs: number, text: string}[]
+
+          for l, line in ipairs(lines) do
+            if not line:find("^%s*$") then
+              indents[#indents + 1] =
+                { line = l, indent = #line:gsub("\t", spaces):match("^%s*"), tabs = #line:match("^\t*"), text = line }
+            end
+          end
+
+          local ret = {} ---@type (Mini.ai.region | {indent: number})[]
+
+          for i = 1, #indents do
+            if i == 1 or indents[i - 1].indent < indents[i].indent then
+              local from, to = i, i
+              for j = i + 1, #indents do
+                if indents[j].indent < indents[i].indent then
+                  break
+                end
+                to = j
+              end
+              from = ai_type == "a" and from > 1 and from - 1 or from
+              to = ai_type == "a" and to < #indents and to + 1 or to
+              local tabs = indents[from].tabs > 0 and indents[from].tabs * vim.o.tabstop or 0
+              ret[#ret + 1] = {
+                indent = indents[i].indent,
+                from = {
+                  line = indents[from].line,
+                  col = ai_type == "a" and 1 or (indents[from].indent + 1 - tabs),
+                },
+                to = { line = indents[to].line, col = #indents[to].text },
+              }
+            end
+          end
+
+          return ret
+        end,
         B = function(ai_type)
           local n_lines = vim.fn.line("$")
           local start_line, end_line = 1, n_lines
@@ -89,7 +128,7 @@ return {
             start_line = first_nonblank == 0 and 1 or first_nonblank
             end_line = last_nonblank == 0 and n_lines or last_nonblank
           end
-        
+
           local to_col = math.max(vim.fn.getline(end_line):len(), 1)
           return { from = { line = start_line, col = 1 }, to = { line = end_line, col = to_col } }
         end,
@@ -109,5 +148,5 @@ return {
         update_n_lines = "gsn", -- Update `n_lines`
       },
     },
-  }
+  },
 }
