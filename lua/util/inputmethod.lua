1fe8b67fc8c3880bdc58ce4d6dd8c4ad0068574d
dongyang <dongyang@wondertek.com.cn>
2024-10-08 17:02:21 +0800

    disable default inputmethod toggle and update packages

--- lua/util/inputmethod.lua
+++ lua/util/inputmethod.lua
@@ -1,6 +1,13 @@
 local M = {}
 
-M.enable_inputmethod = true
+M.enable_inputmethod = false
+
+M.cmd_chinese = "fcitx-remote -s sogoupinyin"
+M.cmd_english = "fcitx-remote -s sogoupinyin"
+if LazyVim.is_win() then
+  M.cmd_chinese = "D:\\local\\bin\\im-select.exe 2052"
+  M.cmd_english = "D:\\local\\bin\\im-select.exe 1033"
+end
 
 function M.toggle(enable)
   if enable == nil then
@@ -9,32 +16,27 @@ function M.toggle(enable)
   M.enable_inputmethod = enable
 end
 
+function M.chinse_input()
+  if M.enable_inputmethod then
+    vim.fn.jobstart(M.cmd_chinese)
+  end
+end
+function M.english_input()
+  if M.enable_inputmethod then
+    vim.fn.jobstart(M.cmd_english)
+  end
+end
+
 function M.setup()
   -- add options for auto switch inputmethod
-  if jit.os:find("Windows") then
-    vim.cmd([[au InsertLeave *  :silent :!D:\\local\\bin\\im-select.exe 1033]])
-    vim.cmd([[au InsertEnter *  :silent :!D:\\local\\bin\\im-select.exe 2052]])
-  elseif vim.fn.executable("fcitx-remote") == 1 then
-    -- vim.cmd([[au InsertLeave *  :silent :!fcitx-remote -s fcitx-keyboard-us]])
-    -- vim.cmd([[au InsertEnter *  :silent :!fcitx-remote -s sogoupinyin]])
-
-    vim.api.nvim_create_autocmd({ "InsertEnter" }, {
-      pattern = { "*" },
-      callback = function()
-        if M.enable_inputmethod then
-          vim.fn.jobstart("fcitx-remote -s sogoupinyin")
-        end
-      end,
-    })
-    vim.api.nvim_create_autocmd({ "InsertLeave" }, {
-      pattern = { "*" },
-      callback = function()
-        if M.enable_inputmethod then
-          vim.fn.jobstart("fcitx-remote -s fcitx-keyboard-us")
-        end
-      end,
-    })
-  end
+  vim.api.nvim_create_autocmd({ "InsertEnter" }, {
+    pattern = { "*" },
+    callback = M.chinse_input,
+  })
+  vim.api.nvim_create_autocmd({ "InsertLeave" }, {
+    pattern = { "*" },
+    callback = M.english_input,
+  })
 end
 
 return M
